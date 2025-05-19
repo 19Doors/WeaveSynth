3a0483026df220915f9478ec29a1dd6e040c99d8
19Doors <sakaarsri1904@gmail.com>
2025-05-19 12:24:46 +0530

    Fix SerpKEY

--- getURLs.py
+++ getURLs.py
@@ -1,11 +1,13 @@
+import os
 from serpapi import search
 
+serp_key = os.getenv("SERPAPI_KEY")
 def get_urls(query):
     params = {
       "engine": "google_news",
       "q": query,
       "hl": "en",
-      "api_key": "84398d532c48aaa41d4c4bdcf339c241ea040e9eb1b707021df2d0cc04e031c8"
+      "api_key": serp_key
     }
 
     search_results = search(params)
